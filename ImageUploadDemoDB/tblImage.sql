05682dc65039cf748cce37fb450667c6ecdb329a
Andrew Swallows <andrew_swallows@yahoo.com>
2020-11-08 15:53:41 -0700

    Missed items in intial commit?

---
+++ ImageUploadDemoDB/tblImage.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblImage]
+(
+	[ImageId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [AlbumImage] IMAGE NOT NULL, 
+    [ImageName] NVARCHAR(50) NOT NULL, 
+    [ImageURL] NVARCHAR(50) NOT NULL, 
+    [ImageAlt] NCHAR(500) NOT NULL
+)
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_tblImage_ImageName_ImageURL] ON [dbo].[tblImage] (ImageName, ImageURL)
---
+++ ImageUploadDemoDB/tblImageTag.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblImageTag]
+(
+	[ImageTagId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [ImageId] UNIQUEIDENTIFIER NOT NULL, 
+    [TagId] UNIQUEIDENTIFIER NOT NULL, 
+    CONSTRAINT [FK_tblImageTag_tblImage] FOREIGN KEY (ImageId) REFERENCES [tblImage]([ImageId]), 
+    CONSTRAINT [FK_tblImageTag_tblTag] FOREIGN KEY (TagId) REFERENCES tblTag(TagId)
+)
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_tblImageTag_ImageId_TagId] ON [dbo].[tblImageTag] (ImageId,TagId)
