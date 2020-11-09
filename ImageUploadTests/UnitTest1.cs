211eacc43be28d7362fc54a6d7985e7db8161589
Andrew Swallows <andrew_swallows@yahoo.com>
2020-11-08 18:08:33 -0700

    Adding unit testing project

---
+++ ImageUploadTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+
+namespace ImageUploadTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
