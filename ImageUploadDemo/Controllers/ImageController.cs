7447c7822bcf421d8e2fe26457774c46db954d63
Andrew Swallows <andrew_swallows@yahoo.com>
2020-11-14 16:00:05 -0700

    Making my presentation layer for the index page dumb and just calling the service/Business logic layer.

--- ImageUploadDemo/Controllers/ImageController.cs
+++ ImageUploadDemo/Controllers/ImageController.cs
@@ -2,23 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AS.ImageAlbum.BusinessLogic.DTO.Query;
 using AS.ImageAlbum.BusinessLogic.Interfaces;
+using AS.ImageAlbum.Website.Models.ImageData;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AS.ImageAlbum.Website.Controllers
 {
     public class ImageController : Controller
     {
-        private IImageService _service;
+        private IImageService service;
 
         public ImageController(IImageService service)
         {
-            this._service = service;
+            this.service = service;
         }
 
         public IActionResult Index()
         {
-            return View();
+            ImageIndex viewModel = new ImageIndex();
+            try
+            {
+                FindAllServicesQuery query = new FindAllServicesQuery();
+                this.service.FindAll(query);
+                if (query.Response == FindAllServicesQuery.SUCCESS)
+                {
+                    
+                    viewModel.AlbumImages = query.AlbumImages;
+                }
+                else
+                {
+                    throw new ArgumentException(FindAllServicesQuery.ERROR, "Business Logic Error");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            return View(viewModel);
         }
     }
 }
--- ImageUploadDemo/Models/ImageData/ImageIndex.cs
+++ ImageUploadDemo/Models/ImageData/ImageIndex.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AS.ImageAlbum.BusinessLogic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace AS.ImageAlbum.Website.Models.ImageData
 {
     public class ImageIndex
     {
+        public List<AlbumImage> AlbumImages { get; set; }
     }
 }
